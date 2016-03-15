ccb55be27ed4cfcf97df34aab21f47eef367c688
katherina26 <thilavanh.katherina@gmail.com>
2016-03-15 10:09:54 -0400

    link styles

--- js/getnav.js
+++ js/getnav.js
@@ -3,6 +3,7 @@ $.get("https://katherina26.github.io/partials/nav.html", function(data) {
   $(document).ready(function() {
 
     $(".container").prepend(data);
+    $("container").fadeIn(data);
 
 
   })
