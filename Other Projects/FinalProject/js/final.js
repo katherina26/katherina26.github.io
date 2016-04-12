d3a3862e781687d6ca9d127fd42f65ebb32cedd1
katherina26 <thilavanh.katherina@gmail.com>
2016-04-11 20:43:59 -0400

    update

--- Other Projects/FinalProject/js/final.js
+++ Other Projects/FinalProject/js/final.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
                                     '</div>'; //col-md-4
                             }) //each dress
 
-                        $("#carData").html(html);
+                        $("#carData").append(html);
 
                     }) //getJSON
             } else if (partial == "orderPage") { //ajax get order.html
